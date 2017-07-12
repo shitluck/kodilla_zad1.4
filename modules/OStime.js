270a9c6267f15ac28cdbcfd4ab469f2dfa27f407
MAc Ryndziewicz <fuji.two6@gmail.com>
2017-07-12 12:00:17 +0200

    add files to a new branch

--- modules/OStime.js
+++ modules/OStime.js
@@ -1,10 +1,8 @@
-var os = require('os');
-
-function convertOStime(uptime) {
-    var hours = Math.floor(uptime/3600);
-    var minutes = Math.floor((uptime%3600)/60);
-    var seconds = Math.floor((uptime%3600)%60);
-    return(hours + 'h', minutes + 'min', seconds + 's');
+function convertOStime(time) {
+    var hours = Math.floor(time/3600);
+    var minutes = Math.floor((time%3600)/60);
+    var seconds = Math.floor((time%3600)%60);
+    return(hours + 'h' + minutes + 'min' + seconds + 's');
 }
 
 exports.print = convertOStime;
