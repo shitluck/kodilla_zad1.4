7e491718eb364d2224262b9baf1c6b4f9c427a0d
MAc Ryndziewicz <fuji.two6@gmail.com>
2017-08-01 13:17:09 +0200

    few changes in code

--- modules/OStime.js
+++ modules/OStime.js
@@ -1,11 +1,19 @@
 function convertOStime(time) {
-    if (time < 0 ) {
-      throw new RangeError("Time should have positive value but got " + time)
-    }
+  if (time < 0 ) {
+    throw new RangeError("Time should have positive value but got " + time)
+  } else if (time < 60) {
+    var seconds = Math.floor((time%3600)%60);
+    return(seconds + ' sek.');
+  } else if (time < 3600) {
+    var minutes = Math.floor((time%3600)/60);
+    var seconds = Math.floor((time%3600)%60);
+    return(minutes + ' min. ' + seconds + ' sek.');
+  } else {
     var hours = Math.floor(time/3600);
     var minutes = Math.floor((time%3600)/60);
     var seconds = Math.floor((time%3600)%60);
     return(hours + ' godz. ' + minutes + ' min. ' + seconds + ' sek.');
+  }
 }
 
 exports.print = convertOStime;
